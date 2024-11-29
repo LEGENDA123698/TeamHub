47c33cf7a0257dad046f2712d27f52fe1417243c
Матвей <hornett@host-192-168-1-3.sohonet.ua>
2024-11-29 21:13:33 +0200

    start page

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,6 @@
+from django.contrib import admin
 from django.urls import path
-from . import views
 
 urlpatterns = [
-    path('', views.StartView.as_view(), name='main-page'),
+    path('admin/', admin.site.urls),
 ]
