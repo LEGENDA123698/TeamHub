08eb639387891c8e4711b8b1b2a308be2ff936b1
Матвей <hornett@host-192-168-1-3.sohonet.ua>
2024-11-29 20:53:53 +0200

    models

--- forum_app/models.py
+++ forum_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Section(models.Model):
     name = models.CharField(max_length=50, blank=True)
@@ -7,7 +8,9 @@ class Section(models.Model):
 class Theme(models.Model):
     name = models.CharField(max_length=50, blank=True)
     text = models.TextField(max_length=10000, blank=True)
+    author = models.ForeignKey(User, related_name="theme_author")
 
 class Message(models.Model):
     text = models.TextField(max_length=10000, blank=True)
+    message_author = models.ForeignKey(User, related_name="message_author")
 
