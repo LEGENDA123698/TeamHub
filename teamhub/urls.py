60410aaba19617c4bd0fb74cc0ef6cba82c38d1d
Матвей <hornett@host-192-168-1-3.sohonet.ua>
2024-11-30 10:29:16 +0200

    -

--- teamhub/urls.py
+++ teamhub/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main_app.urls')),
     path('auth/', include('auth_app.urls')),
+    path('forum/', include('forum_app.urls'))
 ]
