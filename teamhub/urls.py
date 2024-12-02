a9267e67ee4b4053f449b6241c9f8f0e102a3739
Nastya1115 <51201181+Nastya1115@users.noreply.github.com>
2024-12-02 13:53:59 +0100

    Update urls.py

--- teamhub/urls.py
+++ teamhub/urls.py
@@ -21,6 +21,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('main_app.urls')),
+    path('auth/', include('auth_app.urls')),
+    path('forum/', include('forum_app.urls')),
     path('', include('calendar_app.urls')),
     path('', include('materials_app.urls')),
 ]
