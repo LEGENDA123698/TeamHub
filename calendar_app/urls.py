0e297f9e19d67969c5eaf4b1e693f18e353fbf1f
Nastya1115 <51201181+Nastya1115@users.noreply.github.com>
2024-12-02 14:07:36 +0100

    + calendar

---
+++ calendar_app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+import calendar_app.views as views
+
+
+urlpatterns = [
+    path('calendar/', views.calendar_view, name='calendar'),
+    path('calendar/<int:year>/<int:month>/', views.calendar_view, name='calendar_with_date'), # просмотр календаря
+    path("calendar/create_event/", views.EventCreateView.as_view(), name="calendar_create_event"), # создание события
+    path("calendar/update_event/<int:pk>/", views.EventUpdateView.as_view(), name="calendar_update_event"), # обновление события
+    path("calendar/delete_event/<int:pk>/", views.EventDeleteView.as_view(), name="calendar_delete_event"), # удаление события
+]
