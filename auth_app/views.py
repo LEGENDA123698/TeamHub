f260fab021f8af2839b57b08a33a76fa1fcb9fea
Nastya1115 <51201181+Nastya1115@users.noreply.github.com>
2024-12-05 18:31:10 +0100

    Update views.py

--- auth_app/views.py
+++ auth_app/views.py
@@ -14,7 +14,7 @@ def register(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('start')
+            return redirect('main-page')
         else:
             print("FormNotValid")
     else:
@@ -32,7 +32,7 @@ def login_user(request):
             if user is not None:
                 login(request, user)
                 print("login")
-                return redirect('start')
+                return redirect('main-page')
             else:
                 messages.error(request, 'invalid login or password')
     else:
