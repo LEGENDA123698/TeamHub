db01c295bddda5b3fe19b0f06a748ed44e590524
maxseryogin <mserjohin@odessa-gymnasium8.ukr.education>
2024-12-07 15:50:13 +0200

    добавил шаблоны

--- forum_app/models.py
+++ forum_app/models.py
@@ -9,13 +9,9 @@ class Theme(models.Model):
     text = models.TextField(max_length=10000, blank=True)
     author = models.ForeignKey(User, related_name="theme_author", on_delete=models.CASCADE, blank=True, default=1)
     related_section = models.ForeignKey(Section, related_name="section_theme", on_delete=models.CASCADE, blank=True, default=1)
-    author = models.ForeignKey(User, related_name="theme_author", on_delete=models.CASCADE, blank=True, default=1)
-    related_section = models.ForeignKey(Section, related_name="section_theme", on_delete=models.CASCADE, blank=True, default=1)
 
 class Message(models.Model):
     text = models.TextField(max_length=10000, blank=True)
-    message_author = models.ForeignKey(User, related_name="message_author", on_delete=models.CASCADE, blank=True, default=1)
-    related_theme = models.ForeignKey(Theme, related_name="theme_message", on_delete=models.CASCADE, blank=True, default=1)
     message_author = models.ForeignKey(User, related_name="message_author", on_delete=models.CASCADE, blank=True, default=1)
     related_theme = models.ForeignKey(Theme, related_name="theme_message", on_delete=models.CASCADE, blank=True, default=1)
 
