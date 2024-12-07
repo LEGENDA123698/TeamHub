3b61b20a90e87d3d952f97881d6b18b2377ddb50
Матвей <hornett@host-192-168-1-4.sohonet.ua>
2024-12-07 15:31:34 +0200

    -

--- forum_app/urls.py
+++ forum_app/urls.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from .views import *
 
+app_name = 'forum_app'
+
+
 urlpatterns = [
-    path('', SectionListView.as_view(), name="forum-start")
+    path('', SectionListView.as_view(), name="forum-start"),
+    path('section/<int:pk>/', SectionDetailView.as_view(), name='section_detail'),
+    path('theme/<int:pk>/', ThemeDetailView.as_view(), name='theme_detail'),
+
 ]
