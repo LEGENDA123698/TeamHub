2744e154eff2068699b39120437971a853a1a0a4
Матвей <hornett@host-192-168-1-4.sohonet.ua>
2024-12-07 14:03:35 +0200

    -

--- forum_app/urls.py
+++ forum_app/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("", SectionListView.as_view())
+    path('', SectionListView.as_view(), name="forum-start")
 ]
