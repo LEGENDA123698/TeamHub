f375da383cda0a010297b702c9b7ba7bc12f1f7d
Матвей <hornett@host-192-168-1-6.sohonet.ua>
2024-12-13 21:19:47 +0200

    галлерея, небольшие наработки интерфейса в календаре

--- gallery_app/urls.py
+++ gallery_app/urls.py
@@ -5,5 +5,8 @@ from django.conf.urls.static import static
 from .views import *
 
 urlpatterns = [
-    path('', GalleryStartView.as_view(), name='gallery-start')
+    path('', GalleryStartView.as_view(), name='gallery-start'),
+    path('create/', ImageCreateView.as_view(), name='image-create'),
+    path('update/<int:pk>/', ImageUpdateView.as_view(), name='image-update'),
+    path('delete/<int:pk>/', ImageDeleteView.as_view(), name='image-delete'),
 ]
--- gallery_app/views.py
+++ gallery_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from .models import Image
+from django.views.generic import *
+from django.urls import reverse_lazy
+from django.views.generic import DeleteView
+from django.contrib.auth.mixins import UserPassesTestMixin
 
 class GalleryStartView(TemplateView):
     template_name = 'gallery_app/gallery_start.html'
@@ -9,3 +13,29 @@ class GalleryStartView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['images'] = Image.objects.all()
         return context
+
+class ImageCreateView(CreateView):
+    model = Image
+    fields = ['title', 'image', 'grid_column_span', 'grid_row_span']
+    success_url = reverse_lazy('gallery-start')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class ImageUpdateView(UserPassesTestMixin, UpdateView):
+    model = Image
+    fields = ['title', 'image', 'grid_column_span', 'grid_row_span']
+    success_url = reverse_lazy('gallery-start')
+
+    def test_func(self):
+        image = self.get_object()
+        return self.request.user == image.author
+
+class ImageDeleteView(UserPassesTestMixin, DeleteView):
+    model = Image
+    success_url = reverse_lazy('gallery-start')
+
+    def test_func(self):
+        image = self.get_object()
+        return self.request.user == image.author
