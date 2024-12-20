b3b3b5a3d66a198496c46d517a963a6d6117de12
Матвей <hornett@host-192-168-1-6.sohonet.ua>
2024-12-20 13:56:32 +0200

    -

--- gallery_app/views.py
+++ gallery_app/views.py
@@ -5,6 +5,8 @@ from django.views.generic import *
 from django.urls import reverse_lazy
 from django.views.generic import DeleteView
 from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
+from django.core.paginator import Paginator
+from django.shortcuts import redirect
 
 class GalleryStartView(TemplateView):
     template_name = 'gallery_app/gallery_start.html'
@@ -12,12 +14,23 @@ class GalleryStartView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['images'] = Image.objects.all()
+        paginator = Paginator(context['images'], 40)
+        page_number = self.request.GET.get('page')
+        context['page_obj'] = paginator.get_page(page_number)
         return context
 
 class ImageCreateView(LoginRequiredMixin, CreateView):
     model = Image
     fields = ['title', 'image', 'grid_column_span', 'grid_row_span']
     success_url = reverse_lazy('gallery-start')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        response = super().form_valid(form)
+        # Redirect to the page containing the new image
+        page_number = (self.object.pk - 1) // 5 + 1  # Calculate the page number
+        return redirect(f"{self.success_url}?page={page_number}")
+
 
     def form_valid(self, form):
         form.instance.author = self.request.user
