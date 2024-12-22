b49cf0811d1a2082088cb551b3a681f8a3022892
SamuySlitiy <oleksandrsavchenko15@gmail.com>
2024-12-22 15:59:54 +0100

    .

--- notifications_app/views.py
+++ notifications_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render
 from django.views.generic import CreateView, UpdateView, DeleteView, DetailView, ListView
+from django.core.paginator import Paginator
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import *
 from .forms import *
@@ -9,7 +10,6 @@ from .forms import *
     #return user.is_superuser or user.groups.filter(name='Moderators').exists()
 
 # Creation 
-
 class NotificationCreateView(LoginRequiredMixin, CreateView):
     model = Notification
     form_class = NotificationForm
@@ -24,12 +24,12 @@ class NotificationCreateView(LoginRequiredMixin, CreateView):
 
 class NotificationUpdateView(LoginRequiredMixin, UpdateView):
     model = Notification
-    fields = ['text']
     form_class = NotificationForm
     template_name = 'notifications_app/update_notif.html'
     success_url = '/notifications/'
 
 # Delete
+
 
 class NotificationDeleteView(LoginRequiredMixin, DeleteView):
     model = Notification
@@ -41,9 +41,17 @@ class NotificationListView(ListView):
     model = Notification
     template_name = 'notifications_app/notif.html'
     context_object_name = 'notifications'
-    paginate_by = 3
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['notifications'] = Notification.objects.all()
+        paginator = Paginator(context['notifications'], 2)
+        page_num = self.request.GET.get('page')
+        context['page_object'] = paginator.get_page(page_num)
+        return context
 
 class NotificationDetailView(DetailView):
     model = Notification
     template_name = 'notifications_app/notif_detail.html'
     context_object_name = 'notification'
+
